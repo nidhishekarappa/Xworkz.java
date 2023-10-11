87ad0503259f0f2f1b441db6fff0a586bd115566
nidhishekarappa <nidhi.xworkz@gmail.com>
2023-10-12 00:46:21 +0530

    first commit

---
+++ InstagramRunner.java
@@ -0,0 +1,12 @@
+package com.xworkz.nidhi.driver;
+
+import com.xworkz.nidhi.things.Instagram;
+
+public class InstagramRunner {
+
+	public static void main(String[] args) {
+		new Instagram();
+
+	}
+
+}
