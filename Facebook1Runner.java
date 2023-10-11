f10c82a8886f3f3ea1b889504e47bc8bdd36cd13
nidhishekarappa <nidhi.xworkz@gmail.com>
2023-10-12 00:44:16 +0530

    first commit

---
+++ Facebook1Runner.java
@@ -0,0 +1,11 @@
+package com.xworkz.nidhi.driver;
+
+import com.xworkz.nidhi.things.Facebook1;
+
+public class Facebook1Runner {
+
+	public static void main(String[] args) {
+		new Facebook1();
+	}
+
+}
